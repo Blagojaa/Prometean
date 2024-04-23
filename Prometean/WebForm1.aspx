435d9a827da4c7550ddad30e4e54104dd8355d65
Blagoja <Blagoja@DESKTOP-Q93NCJC>
2024-04-23 23:08:38 +0200

    Add project files.

---
+++ Prometean/WebForm1.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Prometean.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+
+    <asp:Label ID="Label1" runat="server" Text="Log in:"></asp:Label>
+    <br /><br />
+    <asp:Label ID="Label2" runat="server" Text="Username"></asp:Label>
+    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+    <br /><br />
+    <asp:Label ID="Label3" runat="server" Text="Password"></asp:Label>
+    <asp:TextBox ID="TextBox2" runat="server" TextMode="Password"></asp:TextBox>
+    <br /><br />
+    <asp:Button ID="Button1" runat="server" Text="Log in:" OnClick="Button1_Click" />
+    <br /><br />
+     <asp:Label ID="Label4" runat="server" Text="Gold cost:" Visible="false"></asp:Label>
+ <br /><br />
+ <asp:Label ID="Label5" runat="server" Text="MythunitsID " Visible="false"></asp:Label>
+ <asp:TextBox ID="TextBox3" runat="server" Visible="false"></asp:TextBox>
+ <br /><br />
+ <asp:Label ID="Label6" runat="server" Text="Gold cost" Visible="false"></asp:Label>
+    <asp:TextBox ID="TextBox4" runat="server" Visible="false"></asp:TextBox>
+ <br /><br />
+ <asp:Button ID="Button2" runat="server" Text="Insert cost" Visible="false" OnClick="Button2_Click"/>
+    <br /><br />
+    <asp:GridView ID="GridView1" runat="server" CssClass="table"></asp:GridView>
+</asp:Content>
+
+
+
+
+
---
+++ Prometean/WebForm1.aspx.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+namespace Prometean
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        SqlConnection con = new SqlConnection("Data Source=(localdb)\\ProjectModels;Initial Catalog=Prometean;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        void GridViewFill(SqlConnection con)
+        {
+            string query = "SELECT * FROM Trupe";
+
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            GridView1.DataSource = reader;
+
+            GridView1.DataBind();
+
+            reader.Close();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (TextBox1.Text == "Blagoja" && TextBox2.Text == "seljak")
+            {
+                Label4.Visible = true;
+                Label5.Visible = true;
+                Label6.Visible = true;
+                TextBox3.Visible = true;
+                TextBox4.Visible = true;
+
+
+                Button2.Visible = true;
+            }
+            try
+            {
+                using (con)
+                {
+                    con.Open();
+
+                    GridViewFill(con);
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Exception Message: " + ex.Message);
+                System.Diagnostics.Debug.WriteLine("Stack Trace: " + ex.StackTrace);
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(TextBox3.Text);
+            int cost = Convert.ToInt32(TextBox4.Text);
+            using (con)
+            {
+                con.Open();
+
+                SqlParameter p1 = new SqlParameter();
+                SqlParameter p2 = new SqlParameter();
+
+
+                p1.Value = id;
+                p2.Value = cost;
+
+                p1.ParameterName = "@id";
+                p2.ParameterName = "@cost";
+
+                string query = "UPDATE Trupe " +
+                                " SET Resursi = @cost " +
+                                " WHERE MythunitsID = @id";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                cmd.Parameters.Add(p1);
+                cmd.Parameters.Add(p2);
+                cmd.ExecuteNonQuery();
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                reader.Close();
+                string query2 = "SELECT * FROM Trupe";
+
+                SqlCommand cmd2 = new SqlCommand(query2, con);
+
+                SqlDataReader reader2 = cmd2.ExecuteReader();
+
+                GridView1.DataSource = reader2;
+
+                GridView1.DataBind();
+
+                reader2.Close();
+
+
+            }
+
+        }
+    }
+}
+
+
